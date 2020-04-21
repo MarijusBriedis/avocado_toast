215276f72fd914a1532a56a0c93942cff0b094f9
Marijus Briedis <marijus.briedis@gmail.com>
2020-04-22 02:50:23 +0300

    All errors in one error

---
+++ bootloader/target/debug/bootloader.d
@@ -0,0 +1,1 @@
+/home/tuxie/git/github.com/avocado_toast/bootloader/target/debug/bootloader: /home/tuxie/git/github.com/avocado_toast/bootloader/src/main.rs
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::process::Command;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    {
+        // Check if NASM is installed and is in the path
+        let valio = Command::new("nasm").arg("-v").output().ok().and_then(|res| {
+            std::str::from_utf8(&res.stdout).ok().and_then(|stdout| {
+                if res.status.success() && stdout.contains("NASM version") {
+                    Some(())
+                } else {
+                    None
+                }
+            })
+        }).ok_or("nasm is not installed or in the path")?;
+    }
+
+    Ok(())
+
 }
