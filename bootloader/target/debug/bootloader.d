0c4d55b76cc87c0f1ac75407b6126c0913f59f64
Marijus Briedis <marijus.briedis@gmail.com>
2020-04-22 02:53:49 +0300

    Remove the files

--- bootloader/target/debug/bootloader.d
+++
@@ -1,1 +0,0 @@
-/home/tuxie/git/github.com/avocado_toast/bootloader/target/debug/bootloader: /home/tuxie/git/github.com/avocado_toast/bootloader/src/main.rs
--- bootloader/target/debug/deps/bootloader-cab7ef9d51fde05e.d
+++
@@ -1,5 +0,0 @@
-/home/tuxie/git/github.com/avocado_toast/bootloader/target/debug/deps/bootloader-cab7ef9d51fde05e: src/main.rs
-
-/home/tuxie/git/github.com/avocado_toast/bootloader/target/debug/deps/bootloader-cab7ef9d51fde05e.d: src/main.rs
-
-src/main.rs:
