b02609b365b2666264c6844f62a45eddca3bc8ee
Marijus Briedis <marijus.briedis@gmail.com>
2020-04-22 00:59:49 +0300

    First steps

---
+++ bootloader/src/stage0.asm
@@ -0,0 +1,16 @@
+[org  0x7c00]
+[bits 16]
+
+entry:
+    mov ax, 0x0003
+    int 0x10
+
+    mov di, 0xb800
+    mov es, di
+    xor di, di
+    mov ax, 0x0f41
+    mov cl, 40
+    rep stosw
+
+    cli
+    hlt
