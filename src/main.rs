0cc569f589a60d641fe85a5775f38e33c943244d
Marijus Briedis <marijus.briedis@gmail.com>
2020-05-12 00:55:51 +0300

    Whatever, lets make a function instead

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,41 @@
 use std::error::Error;
 use std::process::Command;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    macro_rules! check_install {
-        ($command:expr, $args:expr,
-         $expected_in_stdout:expr, $error_message:expr) => {{
-            // Invoke the command
-            Command::new($command)
-                .args($args)
-                .output()
-                .ok()
-                .and_then(|res| {
-                    // Convert the stdout bytes to a string
-                    std::str::from_utf8(&res.stdout).ok().and_then(|stdout| {
-                        // Make sure the command returned successfully
-                        if !res.status.success() {
-                            return None;
-                        }
+fn check_install(command: &str, args: &[&str], expected: &[&str]) -> Option<()> {
+    // Invoke the command
+    let res = Command::new(command).args(args).output().ok()?;
 
-                        // Make sure all expected strings are present in stdout
-                        for expected in $expected_in_stdout {
-                            if !stdout.contains(expected) {
-                                return None;
-                            }
-                        }
-
-                        Some(())
-                    })
-                })
-                .ok_or($error_message)
-        }};
+    // Check if command was successful
+    if !res.status.success() {
+        return None;
     }
 
+    // Convert the stdout bytes to a string
+    let stdout = std::str::from_utf8(&res.stdout).ok()?;
+
+    // Mak sure `stdout` contains everything else
+    if expected.iter().all(|x| stdout.contains(x)) {
+        Some(())
+    } else {
+        None
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     // Check for NASM
-    check_install!(
-        "nasm",
-        &["-v"],
-        &["NASM version"],
-        "nasm not present in the path"
-    )?;
+    check_install("nasm", &["-v"], &["NASM version"]).ok_or("nasm not present in the path")?;
 
     // Check for RUST and targets
     // I should add i586-pc-windows-msvc and x86_64-pc-windows-msvc
-    check_install!(
+    check_install(
         "rustup",
         &["target", "list"],
         &["x86_64-unknown-linux-gnu (installed)"],
-        "RUST targets are not installed"
-    )?;
+    )
+    .ok_or("RUST targets are not installed")?;
 
     // Check for ld.lld
-    check_install!(
-        "ld.lld",
-        &["--version"],
-        &["LLD "],
-        "ld.lld not present in the path"
-    )?;
+    check_install("ld.lld", &["--version"], &["LLD "]).ok_or("ld.lld not present in the path")?;
 
     Ok(())
 }
